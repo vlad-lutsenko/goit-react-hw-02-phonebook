c544cd41dad782f7d24d9a9ee0f5c1ecb029fc0a
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2020-04-20 21:25:05 +0300

    Updates

---
+++ precache-manifest.ac8003eb1f352c348016d98bc864e37d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cdcef5a81191c16f7b182213c706803",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "b72796ab59d30a3109a7",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.0c404c88.chunk.css"
+  },
+  {
+    "revision": "d6348c17d6e0aac32b6d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.435c365e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.435c365e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b72796ab59d30a3109a7",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.5a1a90cc.chunk.js"
+  },
+  {
+    "revision": "bfccad9aa0892efebfa9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.98a8e906.js"
+  }
+]);
